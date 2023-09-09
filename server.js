6b344e009d36a5b7605ccaa31e7d300397a3d5a2
Edward Kim <itgma1231@gmail.com>
2023-09-08 22:33:00 -0400

    sequelize.sync() / User model created/ npm install bcrypt

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+const sequelize = require('./config/connection');
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
     console.log('Listening on Port 3001');
-})
+  });
+});
