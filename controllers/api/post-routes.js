abff1e12844296ce49a0c6245ad9a00b6f64bf8b
Edward Kim <itgma1231@gmail.com>
2023-09-13 13:09:13 -0400

    post-routes.js completed; sign-up-btn styling change

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User, Post } = require('../../models');
 // const withAuth = require('../../utils/auth');
 
+// GET POSTS
 router.get('/', async (req, res) => {
   try {
     const dbPostData = await Post.findAll({
@@ -18,6 +19,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET ONE POST
 router.get('/:id', async (req, res) => {
   try {
     const dbPostData = await Post.findByPk(req.params.id, {
@@ -33,6 +35,56 @@ router.get('/:id', async (req, res) => {
       return;
     }
     res.status(200).json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// POST ~ CREATE NEW POST
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const dbPostData = await Post.create({
+      title,
+      content,
+      user_id: req.session.user_id,
+    });
+    res.status(201).json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// PUT ~ UPDATE POST
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (dbPostData[0] === 0) {
+      res.status(404).json({ message: 'No post was found to update.' });
+      return;
+    }
+    res.status(200).json(dbPostData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// DELETE ~ DELETE POST
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.destroy({
+      where: { id: req.params.id },
+    });
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post was found to delete.' });
+      return;
+    }
+    res.status(204).json(dbPostData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
