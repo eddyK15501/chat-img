1c43ed7d4e4be42ca62d19519f0eac1353a55fa7
Edward Kim <itgma1231@gmail.com>
2023-09-13 15:43:19 -0400

    signup.handlebars with routes setup; css styling added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) => {
   res.status(200).render('homepage');
 });
 
+router.login('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
 module.exports = router;
