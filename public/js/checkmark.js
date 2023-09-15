2251092d1c288d5f1f6df7a359e99550b9a0aa8b
Edward Kim <itgma1231@gmail.com>
2023-09-14 20:35:57 -0400

    i'm not fixing it

--- public/js/checkmark.js
+++ public/js/checkmark.js
@@ -1,10 +1,8 @@
+const check = () => {
+  document.getElementById('todolist-title').classList.toggle('line-through');
+  document.getElementById('todolist-content').classList.toggle('line-through');
+};
 
-    const checkmarkBtns = document.querySelectorAll('#checkmark');
-
-    checkmarkBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            document.getElementById('todolist-title').classList.toggle('line-through');
-            document.getElementById('todolist-content').classList.toggle('line-through');
-        });
-    });
-
+document.querySelectorAll('#checkmark').forEach(btn => {
+    btn.addEventListener('click', check);
+});
