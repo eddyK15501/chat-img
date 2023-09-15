09042c59c6640388d872e4bc56dc11dce7042fde
Edward Kim <itgma1231@gmail.com>
2023-09-15 11:43:27 -0400

    User.js model hooks beforeCreate & beforeUpdate fix

--- models/User.js
+++ models/User.js
@@ -42,11 +42,11 @@ User.init(
   },
   {
     hooks: {
-      async beforeCreate(newUser) {
+      beforeCreate: async (newUser) => {
         newUser.password = await bcrypt.hash(newUser.password, 10);
         return newUser;
       },
-      async beforeUpdate(updatedUser) {
+      beforeUpdate: async (updatedUser) => {
         updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
         return updatedUser;
       },
