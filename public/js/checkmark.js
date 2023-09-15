3bdf63fe68896b78ce74162f09d125f2f22f6ad7
Edward Kim <itgma1231@gmail.com>
2023-09-14 22:53:47 -0400

    checkmark functionality added; new todolist form created

--- public/js/checkmark.js
+++ public/js/checkmark.js
@@ -1,8 +1,15 @@
-const check = () => {
-  document.getElementById('todolist-title').classList.toggle('line-through');
-  document.getElementById('todolist-content').classList.toggle('line-through');
+const check = (event) => {
+  const container = event.currentTarget.parentElement;
+  const title = container.querySelector('.tl-title');
+  const content = container.querySelector('.tl-content');
+
+  title.classList.toggle('text-grey');
+  content.classList.toggle('text-grey');
+  title.classList.toggle('line-through');
+  content.classList.toggle('line-through');
 };
 
-document.querySelectorAll('#checkmark').forEach(btn => {
-    btn.addEventListener('click', check);
+document.querySelectorAll('.checkmark').forEach((btn) => {
+  btn.removeEventListener('click', check);
+  btn.addEventListener('click', check);
 });
--- public/js/todolist.js
+++ public/js/todolist.js
@@ -1,3 +1,22 @@
-// const createTodoList = () => {
+const createTodoList = async (event) => {
+  event.preventDefault();
 
-// }
+  const title = document.getElementById('db-post-title').value.trim();
+  const content = document.getElementById('db-post-content').value.trim();
+
+  if (title && content) {
+    try {
+      const response = await axios.post('/api/posts', {
+        title,
+        content,
+      });
+    } catch (err) {
+        
+    }
+  } else {
+  }
+};
+
+document
+  .getElementById('new-post-btn')
+  .addEventListener('submit', createTodoList);
