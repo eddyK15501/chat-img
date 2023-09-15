70b98bbbeefead373cd0c8dc3c450dd2a744bb0a
Edward Kim <itgma1231@gmail.com>
2023-09-15 11:55:53 -0400

    bcrypt.hash beforeCreate(); individualHooks & returning: true;

--- seeds/newUserData.js
+++ seeds/newUserData.js
@@ -4,7 +4,7 @@ const newUsers = [
   {
     username: 'test',
     email: 'test@gmail.com',
-    password: 'test'
+    password: 'test',
   },
   {
     username: 'test2',
@@ -13,6 +13,10 @@ const newUsers = [
   },
 ];
 
-const seedUsers = () => User.bulkCreate(newUsers);
+const seedUsers = () =>
+  User.bulkCreate(newUsers, {
+    individualHooks: true,
+    returning: true,
+  });
 
 module.exports = seedUsers;
