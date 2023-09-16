2986ab11d1cb4028b9b2e687e603f5ff0693f129
Edward Kim <itgma1231@gmail.com>
2023-09-16 01:19:14 -0400

    user id defined on seeding; reordering bug fix

--- seeds/newUserData.js
+++ seeds/newUserData.js
@@ -2,11 +2,13 @@ const { User } = require('../models');
 
 const newUsers = [
   {
+    id: 1,
     username: 'test',
     email: 'test@gmail.com',
     password: 'test',
   },
   {
+    id: 2,
     username: 'test2',
     email: 'test2@gmail.com',
     password: 'test',
