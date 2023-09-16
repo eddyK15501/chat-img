19e24ef14b14819cca21617d7c5ca0f571d148e1
Edward Kim <itgma1231@gmail.com>
2023-09-15 20:21:00 -0400

    delete.js script tag added to todolist.handlebars; center post-btn when arr.length === 0

---
+++ public/js/delete.js
@@ -0,0 +1,21 @@
+const deletePost = async (event) => {
+  const container = event.currentTarget.parentElement;
+  const postId = parseInt(container.querySelector('.todolist-post-id').innerText, 10);
+
+  try {
+    const response = await axios.delete(`/api/posts/${postId}`);
+
+    if (response.status === 204) {
+      document.location.reload();
+    } else {
+      alert('Failed to delete post.');
+    }
+  } catch (err) {
+    console.log('Error deleting post', err);
+  }
+};
+
+document.querySelectorAll('.delete-btn').forEach((btn) => {
+  btn.removeEventListener('click', deletePost);
+  btn.addEventListener('click', deletePost);
+});
